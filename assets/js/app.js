2de1b2de06932f2ae4791f42aaf2ffacffdbeb8f
Manas P <manas4518pachakkil@gmail.com>
2020-11-29 06:38:56 +0530

    Preloader Test 5

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,9 +10,11 @@ var preloader=bodymovin.loadAnimation({
 //Preloader
 window.addEventListener("load",()=>{
     const preloader=document.querySelector(".preloader");
+    const main=document.querySelector(".main");
     // preloader.delay(5000).fadeOut();
     setTimeout(()=>{
         preloader.classList.add("preloaderFinish");
+        main.classList.add("display");
     },4000);
     // console.log("Finish");
 })
