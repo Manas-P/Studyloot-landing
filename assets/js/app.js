c1add6f94f767bc12a907bf1c0f2bf9e8b602ff1
Manas P <manas4518pachakkil@gmail.com>
2020-11-29 19:12:34 +0530

    Preloader Disabled for Workflow and added Locomotive Scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,26 @@
-// Lottie Animation for Preloader
-var preloader=bodymovin.loadAnimation({
-    container:document.querySelector(".preloader"),
-    renderer:"svg",
-    loop:true,
-    autoplay:true,
-    path:"./assets/animation/preloader.json",
-});
+//Locomotive Scroll
+ const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
+}); 
 
-//Preloader
-window.addEventListener("load",()=>{
-    const preloader=document.querySelector(".preloader");
-    const main=document.querySelector(".main");
-    // preloader.delay(5000).fadeOut();
-    setTimeout(()=>{
-        preloader.classList.add("preloaderFinish");
-        main.classList.add("display");
-    },4000);
-    // console.log("Finish");
-})
+// // Lottie Animation for Preloader
+// var preloader=bodymovin.loadAnimation({
+//     container:document.querySelector(".preloader"),
+//     renderer:"svg",
+//     loop:true,
+//     autoplay:true,
+//     path:"./assets/animation/preloader.json",
+// });
+
+// //Preloader
+// window.addEventListener("load",()=>{
+//     const preloader=document.querySelector(".preloader");
+//     const main=document.querySelector(".main");
+//     // preloader.delay(5000).fadeOut();
+//     setTimeout(()=>{
+//         preloader.classList.add("preloaderFinish");
+//         main.classList.add("display");
+//     },4000);
+//     // console.log("Finish");
+// })
